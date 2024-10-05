16b53a2be6c28e5af50c805d1c4ec649ba4eae44
Wilson Cheung <wcheung1991@gmail.com>
2024-10-05 13:29:31 +0100

    update file naming convention

--- data/housing_prices/scripts/generate_mean_housing_prices.py
+++ data/housing_prices/scripts/generate_mean_housing_prices.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     raw_df = pd.read_csv(file_name)
 
     for eircode_output, _df in raw_df.groupby('Eircode Output'):
-        city = eircode_output.split(': ')[1] if ": " in eircode_output else 'All'
+        routing_key, city = tuple(eircode_output.split(': ')) if ": " in eircode_output else ('IE', 'All')
         preprocessed = _df[['Eircode Output', 'Month', 'VALUE', 'Dwelling Status', 'Type of Buyer']]
         preprocessed.loc[:, ['Year', 'Month']] = preprocessed.Month.str.split(' ', expand=True).rename(columns={0:'Year', 1:'Month'})
-        preprocessed.loc[:, ['Eircode', 'City']] = preprocessed['Eircode Output'].str.split(': ', expand=True).rename(columns={0:'Eircode', 1:'City'})
+        preprocessed.loc[:, ['Routing Key', 'City']] = preprocessed['Eircode Output'].str.split(': ', expand=True).rename(columns={0:'Routing Key', 1:'City'})
         preprocessed.loc[:, 'Month'] = preprocessed.Month.apply(lambda x: strptime(x, '%B').tm_mon)
-        final = preprocessed[['Eircode', 'Month', 'Year', 'Dwelling Status', 'Type of Buyer', 'VALUE']].astype({'VALUE': 'float', 'Year': 'int'})
-        filename = '-'.join(city.split(" "))
-        final.to_csv(f"./data/housing_prices/preprocessed/mean-housing-prices_{filename}.csv")
+        final = preprocessed[['Routing Key', 'City', 'Month', 'Year', 'Dwelling Status', 'Type of Buyer', 'VALUE']].astype({'VALUE': 'float', 'Year': 'int'})
+        final.to_csv(f"./data/housing_prices/preprocessed/mean-housing-prices_{routing_key}.csv", index=False)
